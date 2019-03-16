4582cd73002259de471bc3828a2e63b054a110f4
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-03-16 14:23:23 +0100

    added new .cpp

---
+++ src/Txt_write.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include "Simbody.h"
+#include "Particle_Classes.h"
+#include "Txt_write.h"
+
+void Angle_write(SimTK::State& ref_State, ParticleList& particles) {
+	std::fstream File;
+	SimTK::String filename = "Angles.txt";
+	std::size_t particle_number = particles.getAllParticles().size();
+
+	File.open(filename, std::ios::app);
+	File << ref_State.getQ()[0];
+	
+	for (std::size_t i = 0; i < particle_number; ++i) {
+		File << "\t" << particles[i].getState().getQ()[0];
+	}
+	File << std::endl;
+	File.close();	
+}
