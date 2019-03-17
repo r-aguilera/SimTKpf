b01efb798c8d10b66476a6f51eee981cd60be919
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-03-17 13:53:24 +0100

    txt writing improved a little bit
    Can be much better though

--- src/Txt_write.cpp
+++ src/Txt_write.cpp
@@ -4,16 +4,21 @@
 #include "Txt_write.h"
 
 void Angle_write(SimTK::State& ref_State, ParticleList& particles) {
+
+	std::size_t particle_number = particles.getAllParticles().size();
 	std::fstream File;
 	SimTK::String filename = "Angles.txt";
-	std::size_t particle_number = particles.getAllParticles().size();
-
+	char AngleValue[9];
+	
 	File.open(filename, std::ios::app);
-	File << ref_State.getQ()[0];
+	sprintf_s(AngleValue, "%.4f", to2Pi(ref_State.getQ()[0]));
+	File << AngleValue;
 	
 	for (std::size_t i = 0; i < particle_number; ++i) {
-		File << "\t" << particles[i].getState().getQ()[0];
+		sprintf_s(AngleValue, "\t%.4f", to2Pi(particles[i].getState().getQ()[0]));
+		File << AngleValue;
 	}
+
 	File << std::endl;
 	File.close();	
 }
