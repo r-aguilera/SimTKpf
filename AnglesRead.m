7b5730a7a8199f3d36adcf412ab8da2a0b970448
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-03 23:12:30 +0200

    Added angle-reading Matlab script
    Needs to be optimized

---
+++ AnglesRead.m
@@ -0,0 +1,35 @@
+% Remove variables, clear Command Window and close any figures
+clear;
+clc;
+close all;
+
+% Change these to properly locate the txt file you want to plot
+filename = 'Angles.txt';
+config_type = 'Release';
+build_dir = 'build';
+path = sprintf('%s/%s/%s', build_dir, config_type, filename);
+
+% Read txt
+data = readmatrix('Angles.txt');
+
+% Get matrix sizes
+datasize = size(data);
+iterations = datasize(1);
+statenumber = datasize(2);
+
+% Define axis variables
+Xaxis = []; XaxisRef = [];
+Yaxis = []; YaxisRef = [];
+
+% Assigning values
+for i = 1 : iterations
+    XaxisRef = [XaxisRef i];
+    YaxisRef = [YaxisRef data(i,1)];
+    Xaxis = [Xaxis,linspace(i,i,statenumber-1)];
+    Yaxis = [Yaxis,data(i,2:statenumber)];
+end
+
+% Plot the values
+hold on;
+scatter(Xaxis,Yaxis,'b','.')
+scatter(XaxisRef,YaxisRef,'r','.')
