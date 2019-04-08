da14da4255a6cb7f1700e79dbe5f06e73ec50a7c
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-08 19:09:17 +0200

    New time-counter class
    Inherited from new Measuring_Instrument

---
+++ include/Stopwatch.h
@@ -0,0 +1,26 @@
+#ifndef _STOPWATCH_
+#define _STOPWATCH_
+
+#include "Simbody.h"
+#include "Measuring_Instrument.h"
+
+// Mode variable to decide if measuring real time or CPU time:
+
+enum class StopwatchMode : bool {Real_Time, CPU_Time};
+
+// Class that actually measure time. Member reading is the time elapsed between start() and stop() functions
+
+class Stopwatch : public Measuring_Instrument {
+public:
+	Stopwatch(StopwatchMode);	// Constructor given the mode
+	void start();		// Initiate or reanude the count (take a time reference)
+	void restart();		// Restart the count (reading set to zero)
+	void stop();		// Stop the count (update the reading)
+	void measure() override;	// Just stop() implementation
+
+private:
+	double ref_time;		// Zero time reference from a start() call
+	StopwatchMode mode;
+};
+
+#endif
---
+++ src/Stopwatch.cpp
@@ -0,0 +1,23 @@
+#include "Simbody.h"
+#include "Measuring_Instrument.h"
+#include "Stopwatch.h"
+
+// Stopwatch Implementation
+
+Stopwatch::Stopwatch(StopwatchMode SWmode) : mode(SWmode) { reading = 0; }
+
+void Stopwatch::start() {
+	if (mode == StopwatchMode::Real_Time)		ref_time = SimTK::realTime();
+	else if (mode == StopwatchMode::CPU_Time)	ref_time = SimTK::cpuTime();
+}
+void Stopwatch::restart() { reading = 0; }
+
+void Stopwatch::stop() {
+	measure();
+}
+
+void Stopwatch::measure() {
+	if (mode == StopwatchMode::Real_Time)		reading += SimTK::realTime() - ref_time;
+	else if (mode == StopwatchMode::CPU_Time)	reading += SimTK::cpuTime() - ref_time;
+}
+
