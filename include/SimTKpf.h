d0ef10c8ad98a500e14da64f9fd2a8cc468fec02
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-13 21:14:31 +0200

    Library generalization TODO

---
+++ include/SimTKpf.h
@@ -0,0 +1,12 @@
+#ifndef _SIMTKPF_
+#define _SIMTKPF_
+
+#include "Simbody.h"
+#include "SimTKpf/Particle_Filter.h"
+#include "SimTKpf/Measuring_Instrument.h"
+#include "SimTKpf/Particle_Classes.h"
+#include "SimTKpf/PF_utilities.h"
+#include "SimTKpf/Simbody_Instrument.h"
+#include "SimTKpf/Stopwatch.h"
+
+#endif
