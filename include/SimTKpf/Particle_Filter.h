33617603c1c2ca42111e9cf8b0d13cb0d19dbf78
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-21 20:13:49 +0200

    PF_Options splits from Particle_Filter

--- include/SimTKpf/Particle_Filter.h
+++ include/SimTKpf/Particle_Filter.h
@@ -2,51 +2,10 @@
 #define _PARTICLE_FILTER_
 
 #include "Simbody.h"
+#include "SimTKpf/PF_Options.h"
 #include "SimTKpf/Particle_Classes.h"
 #include "SimTKpf/Simbody_Instrument.h"
 #include "SimTKpf/PF_utilities.h"
-
-// Auxiliary enumeration class for accesing a single particle filter option:
-enum class PF_Options_index {
-	SIMULATION_TIME_STEP,
-	SENSOR_STDDEV,
-	SENSOR_STDDEV_MOD,
-	MOTION_STDDEV,
-	RESAMPLE_STDDEV
-};
-
-// Exception in case of unexpected behaviour using PF_Option_index class:
-class PF_Options_exception : public std::exception {
-	virtual const char* what() const throw(){
-		return "PF_Option_index used does not match existing ones. \nDefault statement reached in switch statements of ParticleFilter.cpp methods:\nsetOption, getOption or updOption";
-	}
-};
-
-// Options class for particle filter algorithm:
-class PF_Options {
-public:
-	PF_Options();
-	// 1 - Time step between two filter iterations
-	// 2 - Standard deviation of Ground Truth sensor's noise 
-	// 3 - Decimal who modifies the sensor standard deviation during updating stage
-	// 4 - Standard deviation of noise added in prediction stage
-	// 5 - Standard deviation of noise added after the resampling stage
-	PF_Options(double, double, double, double, double);
-
-	void setOptions(double, double, double, double, double);
-	void operator =(PF_Options);
-
-	void setOption(PF_Options_index, double);		// Write single option value
-	const double getOption(PF_Options_index) const;	// Return single option as read-only double value
-	double& updOption(PF_Options_index);			// Return single option as writable double value
-
-private:
-	double SIMULATION_TIME_STEP;
-	double SENSOR_STDDEV;
-	double SENSOR_STDDEV_MOD;
-	double MOTION_STDDEV;
-	double RESAMPLE_STDDEV;	
-};
 
 // Main SimTKpf class, for estimation based on particle filter algorithm:
 class ParticleFilter {
--- src/SimTKpf/Particle_Filter.cpp
+++ src/SimTKpf/Particle_Filter.cpp
@@ -1,75 +1,9 @@
 #include "Simbody.h"
+#include "SimTKpf/PF_Options.h"
 #include "SimTKpf/Particle_Classes.h"
 #include "SimTKpf/Particle_Filter.h"
 #include "SimTKpf/PF_utilities.h"
 #include "SimTKpf/Simbody_Instrument.h"
-
-// PF_Options Implementation
-
-PF_Options::PF_Options() {}
-
-PF_Options::PF_Options(	
-	double PF_SIMULATION_TIME_STEP,		// Time step between two filter iterations
-	double PF_SENSOR_STDDEV,			// Standard deviation of Ground Truth sensor's noise 
-	double PF_SENSOR_STDDEV_MOD,		// Decimal who modifies the sensor standard deviation during updating stage
-	double PF_MOTION_STDDEV,			// Standard deviation of noise added in prediction stage
-	double PF_RESAMPLE_STDDEV			// Standard deviation of noise added after the resampling stage
-	) :	SIMULATION_TIME_STEP(PF_SIMULATION_TIME_STEP),
-		SENSOR_STDDEV(PF_SENSOR_STDDEV),
-		SENSOR_STDDEV_MOD(PF_SENSOR_STDDEV_MOD),
-		MOTION_STDDEV(PF_MOTION_STDDEV),
-		RESAMPLE_STDDEV(PF_RESAMPLE_STDDEV) {}
-
-void PF_Options::setOptions(double PF_SIMULATION_TIME_STEP,double PF_SENSOR_STDDEV, double PF_SENSOR_STDDEV_MOD,
-	double PF_MOTION_STDDEV, double PF_RESAMPLE_STDDEV){
-		SIMULATION_TIME_STEP = PF_SIMULATION_TIME_STEP;
-		SENSOR_STDDEV = PF_SENSOR_STDDEV;
-		SENSOR_STDDEV_MOD = PF_SENSOR_STDDEV_MOD;
-		MOTION_STDDEV = PF_MOTION_STDDEV;
-		RESAMPLE_STDDEV = PF_RESAMPLE_STDDEV;
-}
-
-void PF_Options::operator =(PF_Options PF_Options2) {
-	SIMULATION_TIME_STEP = PF_Options2.SIMULATION_TIME_STEP;
-	SENSOR_STDDEV = PF_Options2.SENSOR_STDDEV;
-	SENSOR_STDDEV_MOD = PF_Options2.SENSOR_STDDEV_MOD;
-	MOTION_STDDEV = PF_Options2.MOTION_STDDEV;
-	RESAMPLE_STDDEV = PF_Options2.RESAMPLE_STDDEV;
-}
-
-void PF_Options::setOption(PF_Options_index index, double newValue) {
-	switch (index) {
-		case PF_Options_index::SIMULATION_TIME_STEP:	SIMULATION_TIME_STEP	= newValue;		break;
-		case PF_Options_index::SENSOR_STDDEV:			SENSOR_STDDEV			= newValue;		break;
-		case PF_Options_index::SENSOR_STDDEV_MOD:		SENSOR_STDDEV_MOD		= newValue;		break;
-		case PF_Options_index::MOTION_STDDEV:			MOTION_STDDEV			= newValue;		break;
-		case PF_Options_index::RESAMPLE_STDDEV:			RESAMPLE_STDDEV			= newValue;		break;
-		default: throw PF_Options_exception();
-	}	
-}
-
-const double PF_Options::getOption(PF_Options_index index) const {
-	switch (index) {
-		case PF_Options_index::SIMULATION_TIME_STEP:	return SIMULATION_TIME_STEP;	break;
-		case PF_Options_index::SENSOR_STDDEV:			return SENSOR_STDDEV;			break;
-		case PF_Options_index::SENSOR_STDDEV_MOD:		return SENSOR_STDDEV_MOD;		break;
-		case PF_Options_index::MOTION_STDDEV:			return MOTION_STDDEV;			break;
-		case PF_Options_index::RESAMPLE_STDDEV:			return RESAMPLE_STDDEV;			break;
-		default: throw PF_Options_exception();
-	}
-}
-
-double& PF_Options::updOption(PF_Options_index index) {
-	switch (index) {
-		case PF_Options_index::SIMULATION_TIME_STEP:	return SIMULATION_TIME_STEP;	break;
-		case PF_Options_index::SENSOR_STDDEV:			return SENSOR_STDDEV;			break;
-		case PF_Options_index::SENSOR_STDDEV_MOD:		return SENSOR_STDDEV_MOD;		break;
-		case PF_Options_index::MOTION_STDDEV:			return MOTION_STDDEV;			break;
-		case PF_Options_index::RESAMPLE_STDDEV:			return RESAMPLE_STDDEV;			break;
-		default: throw PF_Options_exception();
-	}
-}
-
 
 // ParticleFilter Implementation
 
