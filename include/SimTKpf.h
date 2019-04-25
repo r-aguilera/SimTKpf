7faf3c24b65d6553973d8cc7fe1d4ab086694e96
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-25 18:18:21 +0200

    Added one missing library reference

--- include/SimTKpf.h
+++ include/SimTKpf.h
@@ -5,6 +5,7 @@
 #include "SimTKpf/Particle_Filter.h"
 #include "SimTKpf/Measuring_Instrument.h"
 #include "SimTKpf/Particle_Classes.h"
+#include "SimTKpf/PF_Options.h"
 #include "SimTKpf/PF_utilities.h"
 #include "SimTKpf/Simbody_Instrument.h"
 #include "SimTKpf/Stopwatch.h"
