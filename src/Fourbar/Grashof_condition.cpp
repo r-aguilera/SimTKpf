b49ec7220844d1d6320ee80149d7484bfd8992a4
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-04-29 19:06:58 +0200

    Finished unoptimized Grashof condition

--- src/Fourbar/Grashof_condition.cpp
+++ src/Fourbar/Grashof_condition.cpp
@@ -9,5 +9,57 @@ bool evaluateGrashof(std::vector<double>& L) {
 	T2 = L[1] + L[4] - L[3] - L[2];
 	T3 = L[4] + L[3] - L[1] - L[2];
 
-	// TODO: Grashof condition evaluation according to terms T1, T2 & T3.
+	// Grashof condition evaluation according to terms T1, T2 & T3.
+	if (T1*T2*T3 == 0) {
+		std::cout << "Fourbar is a Crank-Crank and it folds." << std::endl;
+		isInputBarACrank = true;
+	} 
+	else {
+		if (T1 < 0) {
+			if (T2 < 0) {
+				if (T3 < 0) {
+					std::cout << "Fourbar is a 0-Rocker-0-Rocker." << std::endl;
+					isInputBarACrank = false;
+				}
+				else {
+					std::cout << "Fourbar is a Crank-Crank." << std::endl;
+					isInputBarACrank = true;
+				}
+			}
+			else {
+				if (T3 < 0) {
+					std::cout << "Fourbar is a Rocker-Rocker." << std::endl;
+					isInputBarACrank = false;
+				}
+				else {
+					std::cout << "Fourbar is a Pi-Rocker-Pi-Rocker." << std::endl;
+					isInputBarACrank = false;
+				}
+			}
+		}
+		else {
+			if (T2 < 0) {
+				if (T3 < 0) {
+					std::cout << "Fourbar is a Rocker-Crank." << std::endl;
+					isInputBarACrank = false;
+				}
+				else {
+					std::cout << "Fourbar is a Pi-Rocker-0-Rocker." << std::endl;
+					isInputBarACrank = false;
+				}
+			}
+			else {
+				if (T3 < 0) {
+					std::cout << "Fourbar is a 0-Rocker-Pi-Rocker." << std::endl;
+					isInputBarACrank = false;
+				}
+				else {
+					std::cout << "Fourbar is a Crank-Rocker." << std::endl;
+					isInputBarACrank = true;
+				}
+			}
+		}
+	}
+
+	return isInputBarACrank;
 }
