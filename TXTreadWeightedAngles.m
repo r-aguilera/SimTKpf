f31071b78fb9f868df58dec68bdbb0afcc95eb2b
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-10-17 00:04:27 +0200

    weighted angles plot script
    optimization needed.
    scatter() substitution?

---
+++ TXTreadWeightedAngles.m
@@ -0,0 +1,42 @@
+% Remove variables, clear Command Window and close any figures
+clear;
+clc;
+close all;
+
+% Change these to properly locate the txt files you want to plot
+Angle_filename = 'Angles.txt';
+Weight_filename = 'Weights.txt';
+config_type = 'Release';
+build_dir = 'build';
+Angle_path = sprintf('%s/%s/%s', build_dir, config_type, Angle_filename);
+Weight_path = sprintf('%s/%s/%s', build_dir, config_type, Weight_filename);
+
+% Read txt
+Angle_data = readmatrix(Angle_path);
+Weight_data = readmatrix(Weight_path);
+
+% Get matrix sizes
+datasize = size(Angle_data);
+iterations = datasize(1);
+statenumber = datasize(2);
+
+hold on
+for i = 1: iterations
+    angles = Angle_data(i,:);
+    weights = Weight_data(i,2:statenumber);
+    
+    maxW = max(weights);
+    minW = min(weights);
+    
+    if maxW == minW
+        for j = 1 : statenumber-1 
+            scatter(i, angles(1+j), '.', 'b')
+        end
+    else
+        for j = 1 : statenumber -1
+            scatter(i, angles(1+j), '.', 'b', 'MarkerEdgeAlpha', (weights(j)-minW)/(maxW-minW))
+        end        
+    end
+    
+    scatter(i, angles(1), '.','r')
+end
