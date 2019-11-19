cb5cf8b2d96ae3294ade79063cfe0d5c4ec65c76
Raúl Aguilera López <47693286+r-aguilera@users.noreply.github.com>
2019-11-19 12:49:38 +0100

    added flags
    Fourbar_NO_OUTPUT.cpp became redundant and has been deleted.
    Minor general changes were done too.

--- src/Fourbar/Fourbar.cpp
+++ src/Fourbar/Fourbar.cpp
@@ -1,7 +1,3 @@
-//#include <cstdio>		// Already included in "Simbody.h"
-//#include <array>		// Already included in "Simbody.h"
-//#include <cmath>		// Already included in "Simbody.h"
-//#include <algorithm>	// Maybe useful in future
 #include "Simbody.h"
 #include "SimTKpf.h"
 #include "Fourbar/assemble_Fourbar.h"
@@ -9,11 +5,8 @@
 #include "Fourbar/Txt_write.h"
 #include "Fourbar/Grashof_condition.h"
 
-int main(int argc, char**argv)
+int main(int argc, char *argv[])
 {
-	//TODO: Procesar argc/argv para ver flags:
-	// "--quiet"
-	bool is_quiet = false;
 
 	double SIMULATION_TIME = 60;
 	double SIM_TIME_STEP = 0.006;
@@ -29,7 +22,7 @@ int main(int argc, char**argv)
 		RESAMPLE_STDDEV
 	);
 	std::size_t PARTICLE_NUMBER = 200;
-	ParticleFilter FILTER(PARTICLE_NUMBER, PARTICLE_FILTER_OPTIONS);
+	ParticleFilter filter(PARTICLE_NUMBER, PARTICLE_FILTER_OPTIONS);
 	
 	std::vector <double> BAR_LENGHTS = { // Examples: Double Crank: (2, 4, 3, 4), Crank-Rocker: (4, 2, 3, 4)
 		2,		// Bar1 lenght
@@ -37,9 +30,19 @@ int main(int argc, char**argv)
 		3,		// Bar3 lenght
 		4 };	// Bar4 lenght
 	GrashofCondition FOURBAR_CONFIGURATION = evaluateGrashof(BAR_LENGHTS);
-	const bool TXT_WRITING_IS_ENABLED = false;
+
+	bool OUTPUT_IS_ENABLED = true;
+	bool PAUSE_IS_ENABLED = true;
+	bool TXT_WRITING_IS_ENABLED = false;
 
 	try {
+		// Handle flags
+		for (int i = 1; i < argc; ++i) {
+			if (strcmp("--quiet", argv[i])==0)			OUTPUT_IS_ENABLED = false;
+			else if (strcmp("--nopause", argv[i])==0)	PAUSE_IS_ENABLED = false;
+			else if (strcmp("--txtwrite", argv[i])==0)	TXT_WRITING_IS_ENABLED = true;
+		}
+
 		// Create the system.
 		SimTK::MultibodySystem system;
 		SimTK::SimbodyMatterSubsystem matter(system);
@@ -68,9 +71,11 @@ int main(int argc, char**argv)
 		assembler.lockMobilizer(Bar1.getMobilizedBodyIndex());
 
 		// We will random assign the reference state and particles states
-		std::cout << FOURBAR_CONFIGURATION.get_description() << "\n\nAssigning states and assembling fourbar..." << std::endl;
-		assemble_Fourbar(FOURBAR_CONFIGURATION, system, assembler, RefState, FILTER);
-		FILTER.setEqualWeights();
+		if (OUTPUT_IS_ENABLED) {
+			std::cout << FOURBAR_CONFIGURATION.get_description() << "\n\nAssigning states and assembling fourbar..." << std::endl;
+		}
+		assemble_Fourbar(FOURBAR_CONFIGURATION, system, assembler, RefState, filter);
+		filter.setEqualWeights();
 		
 		// Simulate it.
 		SimTK::RungeKuttaMersonIntegrator integ(system);
@@ -82,7 +87,7 @@ int main(int argc, char**argv)
 		pargyr.reserve(PARTICLE_NUMBER);
 
 		for (std::size_t i = 0; i < PARTICLE_NUMBER; i++) {							// Arrange gyroscope vector
-			pargyr.push_back(Gyroscope(system, matter, FILTER.updParticleVec()[i].updState(), BAR_LENGHTS[0], 0));
+			pargyr.push_back(Gyroscope(system, matter, filter.updParticleVec()[i].updState(), BAR_LENGHTS[0], 0));
 		}
 		Gyroscope::setGlobalSeed(getSeed());
 
@@ -97,56 +102,79 @@ int main(int argc, char**argv)
 
 		for (double time = 0; time <= SIMULATION_TIME; time += SIM_TIME_STEP) {	// Loop to slowly advance simulation
 			
-			Sw_total_time.stop();
+			Sw_total_time.start();	// Start the total CPU time measure
+
+			if(OUTPUT_IS_ENABLED){
 			std::cout << "\n NEXT ITERATION...\n\nCurrent real time: " << time << " s\nCurrent CPU time: "
 				<< Sw_total_time.read()  << " s" << std::endl;
-			Sw_total_time.start();
-			
+			}			
+
 			if (TXT_WRITING_IS_ENABLED) {
-				Angle_write(RefState, FILTER.updParticleList());
-				Weight_write(FILTER.updParticleList());
+				Angle_write(RefState, filter.updParticleList());
+				Weight_write(filter.updParticleList());
 				Omega_write(gyr, pargyr);
 			}
-			Sw_ref_advance.restart();
-			Sw_ref_advance.start();
-			advance(RefState, ts, SIM_TIME_STEP);					// Advance reference state
-			Sw_ref_advance.stop();
-			Sw_part_advance.restart();
-			Sw_part_advance.start();
-			FILTER.updateStates(ts, assembler);						// Advance particles state
-			Sw_part_advance.stop();
+
+			Sw_ref_advance.start();					// Start the reference state advance time measure
+			advance(RefState, ts, SIM_TIME_STEP);	// Advance the reference state
+			Sw_ref_advance.stop();					// Stop the reference state advance time measure
+
+			Sw_part_advance.start();			// Start the particles state advance time measure
+			filter.updateStates(ts, assembler);	// Advance particles state
+			Sw_part_advance.stop();				// Stop the particles state advance time measure
+
 			gyr.measure();											// Update reference gyroscope reading
-			FILTER.updateWeights<Gyroscope>(gyr.read(), pargyr);	// Update particles gyroscope reading and weights
-			FILTER.calculateESS();									// Calculate particles ESS
+			filter.updateWeights<Gyroscope>(gyr.read(), pargyr);	// Update particles gyroscope reading and weights
+			filter.calculateESS();									// Calculate particles ESS
 
-			printf("\nPARTICLE SUMMARY:\n");
-			for (std::size_t i = 0; i < PARTICLE_NUMBER; i++)
-				printf("\nParticle %3.u \tOmega =%9.5f\tLog(w) = %10.5f\tAngle = %7.3f",
-					static_cast<unsigned int>(i + 1),								static_cast<double>(pargyr[i].read()),
-					static_cast<double>(FILTER.updParticleVec()[i].getWeight()),	static_cast<double>(to2Pi(FILTER.updParticleVec()[i].getState().getQ()[0]))
-				);
-			std::cout << "\n\nReference Gyroscope: " << gyr.read() << std::endl;
-			std::cout << "Reference Angle: " << to2Pi(RefState.getQ()[0]) << std::endl;
-			std::cout << "\nReference State advance time: " << Sw_ref_advance.read() <<  " s" << std::endl;
-			std::cout << "Parrticles States advance time: " << Sw_part_advance.read() << " s" << std::endl;
-			printf("\n ESS = %f %%\n", static_cast<double>(FILTER.getESS() * 100));
-
-			if (FILTER.getESS() < 0.5) {		// Resample when < 50 % of effective particles
-				Sw_resample.start();
-				FILTER.resample();
-				Sw_resample.stop();
-				for (std::size_t i = 0; i < PARTICLE_NUMBER; i++) {	// Noise added to angular velocity
-					double Rate = Bar1.getRate(FILTER.updParticleVec()[i].updState());
-					Bar1.setRate(FILTER.updParticleVec()[i].updState(), Rate + resample_noise.getValue());
-				}
-				std::cout << "\nResample done! " << "Resample time: " << Sw_resample.read() << std::endl;
-				Sw_resample.restart();
+			if(OUTPUT_IS_ENABLED){
+				printf("\nPARTICLE SUMMARY:\n");
+				for (std::size_t i = 0; i < PARTICLE_NUMBER; i++)
+					printf("\nParticle %3.u \tOmega =%9.5f\tLog(w) = %10.5f\tAngle = %7.3f",
+						static_cast<unsigned int>(i + 1),								static_cast<double>(pargyr[i].read()),
+						static_cast<double>(filter.updParticleVec()[i].getWeight()),	static_cast<double>(to2Pi(filter.updParticleVec()[i].getState().getQ()[0]))
+					);
+				std::cout << "\n\nReference Gyroscope: " << gyr.read() << std::endl;
+				std::cout << "Reference Angle: " << to2Pi(RefState.getQ()[0]) << std::endl;
+				std::cout << "\nReference State advance time: " << Sw_ref_advance.read() <<  " s" << std::endl;
+				std::cout << "Particles States advance time: " << Sw_part_advance.read() << " s" << std::endl;
+				printf("\n ESS = %f %%\n", static_cast<double>(filter.getESS() * 100));
 			}
-			std::cout << "\nPress Enter to keep iterating" << std::endl;
+
+			if (filter.getESS() < 0.5) {		// Resample when < 50 % of effective particles
+				
+				Sw_resample.start();	// Start the reample time measure
+				filter.resample();		// Resample
+				Sw_resample.stop();		// Stop the resample time meause
+				
+				// Add noise to angular velocity
+				for (std::size_t i = 0; i < PARTICLE_NUMBER; i++) {	
+					double Rate = Bar1.getRate(filter.updParticleVec()[i].updState());
+					Bar1.setRate(filter.updParticleVec()[i].updState(), Rate + resample_noise.getValue());
+				}
+				if (OUTPUT_IS_ENABLED) {
+					std::cout << "\nResample done! " << "Resample time: " << Sw_resample.read() << std::endl;
+				}
+				Sw_resample.restart(); // Refresh the resample time measuse
+			}
+			if (PAUSE_IS_ENABLED) {
+				if (OUTPUT_IS_ENABLED) {
+					std::cout << "\nPress Enter to keep iterating" << std::endl;
+				}
+				getchar();
+			}
+
+			Sw_ref_advance.restart();	// Refresh the reference state advance time measure
+			Sw_part_advance.restart();	// Refresh the particles state advance time measure
+			Sw_total_time.stop();		// Stop the total CPU time measure
+		}
+
+		if(PAUSE_IS_ENABLED){
+			if (OUTPUT_IS_ENABLED) {
+				std::cout << "\nSIMULATION ENDED. Press Enter to close this window." << std::endl;
+			}
 			getchar();
 		}
-		std::cout << "\nSIMULATION ENDED. Press Enter to close this window." << std::endl;
-		getchar();
 	}
 	catch (const std::exception& e) {
 		std::cout << "Error: " << e.what() << std::endl;
